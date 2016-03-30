b4f2c0cea2c026da184f55f790186b0176e7a2e2
Eric Pan <erzpan@gmail.com>
2016-03-30 17:03:00 -0500

    initial commit

---
+++ Basketball/AHK/Test.ahk
@@ -0,0 +1,6 @@
+﻿Click 2724, 540
+Click 2724, 540
+Sleep, 1000
+Click down 2724, 540
+Sleep, 100
+Click up 2257, 535
---
+++ Basketball/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoHotKeyNET;
+
+namespace Basketball
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AutoHotKey.PathEXE = @"Exe\AutoHotkeyU64.exe";
+			while (true)
+			{
+				Console.Write("Input command: ");
+				string raw = Console.ReadLine();
+				string input = raw.ToUpper();
+				switch (input)
+				{
+					case "CURSOR":
+						Point pt = Cursor.Position;
+						Console.WriteLine("Cursor: ({0}, {1})", pt.X, pt.Y);
+						break;
+					case "TEST":
+						Cursor.Position = new Point(2171, 382);
+						AutoHotKey.RunAHK(@"AHK\MouseLeftClick");
+						Thread.Sleep(500);
+						Cursor.Position = new Point(2171, 382);
+						AutoHotKey.RunAHK(@"AHK\MouseLeftClick");
+
+						//Cursor.Position = new Point(2724, 540);
+						//AutoHotKey.RunAHK(@"AHK\MouseLeftClick");
+						//AutoHotKey.RunAHK(@"AHK\MouseLeftClick");
+						//Cursor.Position = new Point(2724, 539);
+						//Thread.Sleep(1000);
+						//AutoHotKey.RunAHK(@"AHK\MouseLeftDown");
+						//Thread.Sleep(100);
+						//Cursor.Position = new Point(2257, 535);
+						//AutoHotKey.RunAHK(@"AHK\MouseLeftUp");
+						break;
+					case "QUIT":
+						Console.WriteLine("Press any key to exit.");
+						Console.ReadKey();
+						return;
+					default:
+						Console.WriteLine("Invalid input.");
+						break;
+				}
+			}
+		}
+	}
+}
